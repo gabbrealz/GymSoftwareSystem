c8bf5518378732a7bd2c43346ecaf5e51686c84f
unknown <christianagot11@gmail.com>
2026-02-16 11:41:29 +0800

    disabled sanctum's redirect when user is not authenticated

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -14,5 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function ($request, Throwable $e) {
+            if ($e instanceof AuthenticationException) {
+                return new JsonResponse(['message' => 'You are not logged in'], 401);
+            }
+            return null;
+        });
     })->create();
