a0eac4a7c6954eb85b66dbe3ffe25e0c8d76bc54
unknown <christianagot11@gmail.com>
2026-02-17 01:49:36 +0800

    removed sanctum configs for sessions. modified web.php route fallback to not use session middleware

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Session\Middleware\StartSession;
 
 Route::fallback(function () {
     return response()->json([
         'message' => 'Endpoint not found.'
     ], 404);
-});
+})->withoutMiddleware([StartSession::class]);
