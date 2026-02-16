ff4bfc2aed2add73d07dafd9bfb87fef7dad3a49
unknown <christianagot11@gmail.com>
2026-02-16 22:10:48 +0800

    removed configurations that won't be used for this project

--- backend/config/cache.php
+++ backend/config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_STORE', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -37,14 +37,6 @@ return [
         'array' => [
             'driver' => 'array',
             'serialize' => false,
-        ],
-
-        'database' => [
-            'driver' => 'database',
-            'connection' => env('DB_CACHE_CONNECTION'),
-            'table' => env('DB_CACHE_TABLE', 'cache'),
-            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
-            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
         ],
 
         'file' => [
