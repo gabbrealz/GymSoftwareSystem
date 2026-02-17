be12e50e8847a570301f44f5071c5a5d460154ba
unknown <christianagot11@gmail.com>
2026-02-18 01:59:32 +0800

    modified Manager and Employee seeders

--- backend/database/seeders/ManagerSeeder.php
+++ backend/database/seeders/ManagerSeeder.php
@@ -14,7 +14,29 @@ class ManagerSeeder extends Seeder
             'username' => 'Ariana',
             'email' => 'ariana@sertfit.com',
             'password' => '1234567A',
-            'address' => 'my.loveri kingdom',
+            'address' => 'San Isidro',
+            'contact_number' => '09123456789',
+            'hire_date' => Carbon::now(),
+            'monthly_salary' => 50000.00,
+            'role' => 'Manager',
+        ]);
+
+        Employee::create([
+            'username' => 'Christian',
+            'email' => 'christian@sertfit.com',
+            'password' => '1234567A',
+            'address' => 'Malibay',
+            'contact_number' => '09123456789',
+            'hire_date' => Carbon::now(),
+            'monthly_salary' => 50000.00,
+            'role' => 'Manager',
+        ]);
+
+        Employee::create([
+            'username' => 'June',
+            'email' => 'june@sertfit.com',
+            'password' => '1234567A',
+            'address' => 'Tenement, Calamansi St.',
             'contact_number' => '09123456789',
             'hire_date' => Carbon::now(),
             'monthly_salary' => 50000.00,
