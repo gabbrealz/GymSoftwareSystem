99a29748963b43e6ed8a4ded0cab61615074610f
unknown <christianagot11@gmail.com>
2026-02-17 16:01:00 +0800

    added expiration date to the auth token

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Employee;
@@ -22,7 +23,7 @@ class AuthController extends Controller
                 return response()->json(['message' => 'Invalid credentials'], 401);
             }
     
-            $token = $employee->createToken('api-token')->plainTextToken;
+            $token = $employee->createToken('sertfit-api-token', expiresAt: Carbon::now()->addHour())->plainTextToken;
         }
         catch (ValidationException $e) {
             return response()->json(['message' => 'Validation failed'], 422);
