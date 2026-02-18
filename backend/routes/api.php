a2295a2d81e2abf54b876aba55802e91b40b789d
unknown <christianagot11@gmail.com>
2026-02-18 11:57:06 +0800

    added the new api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\GymLogController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -16,6 +18,15 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('/employees', [ManagerController::class, 'create_employee']);
     Route::put('/employees/{employee}', [ManagerController::class, 'update_employee']);
     Route::delete('/employees/{employee}', [ManagerController::class, 'delete_employee']);
+
+    Route::get('/members', [MemberController::class, 'get_members']);
+    Route::post('/members', [MemberController::class, 'create_member']);
+    Route::put('/members/{member}', [MemberController::class, 'update_member']);
+    Route::delete('/members/{member}', [MemberController::class, 'delete_member']);
+
+    Route::get('/workout-sessions', [GymLogController::class, 'get_logs']);
+    Route::post('/workout-sessions/{workoutSession}', [GymLogController::class, 'create_log']);
+    Route::delete('/workout-sessions/{workoutSession}', [GymLogController::class, 'delete_log']);
 });
 
 Route::fallback(function () {
